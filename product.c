22e6182391a2b9f933f47f8eec01ac5e8320dba7
2junh22 <junehee0205@naver.com>
2020-04-19 16:44:40 +0900

    find problem in product.c main- Fixed

--- product.c
+++ product.c
@@ -7,13 +7,13 @@ int selectmenu(){
     printf("1. 조회\n");
     printf("2. 추가\n");
     printf("3. 수정\n");
-    printf("4. 삭제\n");
-    printf("0. 종료\n\n");
+    printf("4. 삭제\n")
     printf("5. 파일저장\n");
     printf("6. 이름검색\n");
     printf("7. 무게검색\n");
     printf("8. 가격검색\n");
     printf("9. 별점검색\n");
+    printf("0. 종료\n\n");
     printf("=> 원하는 메뉴는? ");
     scanf("%d", &menu);
     return menu;
