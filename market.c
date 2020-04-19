ba5f6e4bd50b6ee9affe1d0d8f3b347c0b17aafd
2junh22 <junehee0205@naver.com>
2020-04-19 16:35:19 +0900

    Added Loaddata to market main

---
+++ market.c
@@ -0,0 +1,54 @@
+#include "manager.h"
+Product plist[100];
+#define DEBUG
+    
+    
+int main(void){
+#ifdef DEBUG
+    printf("Debug: %s %s %s %d\n", __DATE__, __TIME__, __FILE__, __LINE__);
+   
+#endif
+
+    count = loadData(plist);  
+    curcount = count;
+
+    while (1){
+        menu = selectmenu();
+        if (menu == 0) break;
+        
+        if (menu == 1){
+            if (count > 0){
+                listProduct(plist,curcount);
+            }
+            else{ printf("데이터가 없습니다\n");
+            }
+        }
+        else if (menu == 2){count += addProduct(&plist[curcount++]);}
+        else if (menu == 3){
+            int no = selectDataNo(plist, curcount);
+            if(no ==0){
+                printf("=> 취소됨!\n");
+                continue;
+            }
+            updateProduct(&plist[no-1]);
+        }
+        else if (menu == 4){
+            int no = selectDataNo(plist, curcount);
+            if(no == 0){
+                printf("=> 취소됨!\n");
+                continue;
+            }
+            int deleteok;
+            printf("정말로 삭제하시겠습니까?(삭제:1)");
+            getchar();
+            scanf("%d",&deleteok);
+            if(deleteok == 1){
+                if(deleteProduct(&plist[no-1])){
+                count --;}
+            }
+        }
+        
+    }
+    printf("종료됨!\n");
+    return 0;
+}
